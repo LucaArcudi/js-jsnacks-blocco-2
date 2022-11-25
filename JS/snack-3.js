2a7b0fb32a099b815c961024bfcbac5ed38ed87a
LucaArcudi <arcudilu@gmail.com>
2022-11-25 17:42:35 +0100

    sol snack 3 max-value

--- JS/snack-3.js
+++ JS/snack-3.js
@@ -4,8 +4,14 @@ let numbers = [2, 4, 6, 12, 8, 10, 14, 16, 18, 20];
 
 let somma = 0;
 
+let maxValue = numbers[0];
+
 for(let i = 0; i < 10; i++){
     somma += numbers[i];
+    
+    if (maxValue < numbers[i]){
+        maxValue = numbers[i]
+    }
 }
 
 console.log(somma);
@@ -15,3 +21,11 @@ console.log(somma);
 let media = somma / 10;
 
 console.log(media);
+
+// valore massimo primi 10 elementi
+
+console.log(maxValue);
+
+
+
+
