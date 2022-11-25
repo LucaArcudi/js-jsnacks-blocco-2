3cbbeab12a1845ff4b29377132f4612a6bb8a9e7
LucaArcudi <arcudilu@gmail.com>
2022-11-25 15:43:52 +0100

    ...

---
+++ JS/snack-1.js
@@ -0,0 +1,16 @@
+// Crea un array vuoto.
+// Chiedi per 6 volte all'utente di inserire un numero,
+// se è dispari inseriscilo nell'array.
+
+let numbers = [];
+
+let inputNumber = parseInt(prompt("Inserisci un numero"));
+
+for (i>0; i < numbers.length; i++){
+    inputNumber == numbers[i];
+}
+
+if (inputNumber % 1){
+    numbers.push(inputNumber);
+    console.log(numbers);
+}
