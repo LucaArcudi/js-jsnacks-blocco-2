49948ed4ac808d3f672526cdcdc8db6c1fecf523
LucaArcudi <arcudilu@gmail.com>
2022-11-25 16:05:56 +0100

    snack-2

---
+++ JS/snack-2.js
@@ -0,0 +1,10 @@
+// Inserisci un numero,
+// se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+let number = parseInt (prompt("Inserisci un numero"));
+
+if (number % 2 = 0){
+    console.log(number);
+} else {
+    console.log(number + 1);
+}
