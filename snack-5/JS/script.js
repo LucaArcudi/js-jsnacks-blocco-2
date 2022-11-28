a83814016cb6ce45f7d2123738c39b1a238ba744
LucaArcudi <arcudilu@gmail.com>
2022-11-28 11:50:47 +0100

    snack 5

---
+++ snack-5/JS/script.js
@@ -0,0 +1,35 @@
+/**
+ *
+ * Generatore di persone fittizie casuali:
+ * Il Grande Gatsby ha una lista di nomi e una lista di cognomi, da queste vuole generare una falsa lista di invitati con nome e cognome.
+ *
+ */
+
+const names = ['Michela', 'Giuseppina', 'Taylor','Donna', 'Marsha', 'Fabio', 'Roberto'];
+const lastnames = ['Cappelletti', 'Jager', 'Porcinelli' , 'Buffetti' , 'Forghieri', 'Papagni', 'Marazzini'];
+
+
+let guests = [];
+
+while (guests.length < 10){
+
+        
+    
+    let randomName = Math.floor(Math.random() * (names.lenght));
+    let randomLastnames = Math.floor(Math.random() * (lastnames.lenght));
+
+    let guest = names[randomName] + "" + lastnames[randomLastnames];
+
+    guests.push(guest);
+    
+
+} 
+
+console.log(guests);
+
+
+
+
+
+
+
